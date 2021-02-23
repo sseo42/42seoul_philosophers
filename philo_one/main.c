f775a8bdf073609fb433e41b695ca8f698257486
Sungmin Seo <sseo@c4r7s4.42seoul.kr>
2021-02-23 20:34:07 +0900

    update

--- philo_one/main.c
+++ philo_one/main.c
@@ -2,14 +2,14 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <time.h>
 
-# define REFLESH_TIME_INTERVAL 3600000000
-
+static const long long REFLESH_TIME_INTERVAL = 3600000000;
 static long long	number_of_philosophers = 4;
-static long long	time_to_die = 500;
-static long long	time_to_eat = 20;
-static long long	time_to_sleep = 10;
+static long long	time_to_die = 500000;
+static long long	time_to_eat = 2000;
+static long long	time_to_sleep = 1000;
 pthread_mutex_t		mutex_test[4];
 long long			last_meal[4];
 long long			current_time[4];
@@ -20,7 +20,7 @@ void		*eatNsleep(void *num)
 	long long		left;
 	long long		right;
  
-	my_number = &(long long *)(num);
+	my_number = *(long long *)(num);
 	left = (!my_number) ? number_of_philosophers : my_number - 1;
 	right = (my_number + 1 >= number_of_philosophers) ? 0 : my_number + 1;
 	while (current_time[my_number] - last_meal[my_number] < time_to_die)
@@ -47,10 +47,11 @@ void		*philosopher(void *num)
 	pthread_t		hand;
 	long long		my_number;
 
-	my_number = &(long long *)(num);
+	my_number = *(long long *)(num);
+	printf("%lld\n", my_number);
 	gettimeofday(&mytime, NULL);
 	current_time[my_number] = mytime.tv_usec;
-	last_mea[my_number] = current_time[my_number];
+	last_meal[my_number] = current_time[my_number];
 	pthread_create(&hand, NULL, eatNsleep, num);
 	pthread_detach(hand);
 	while (current_time[my_number] - last_meal[my_number] < time_to_die)
@@ -64,20 +65,25 @@ void		*philosopher(void *num)
 		}
 		current_time[my_number] = mytime.tv_usec;
 	}
-	printf("%d died\n", my_number);
+	printf("%lld died\n", my_number);
 	return (num);
 }
 
 int			main(void)
 {
-	pthread_t	ppl[number_of_philosoplers];
+	pthread_t	ppl[number_of_philosophers];
 	long long	i;
 
 	i = 0;
+	while (i < number_of_philosophers)
+	{
+		pthread_create(&(ppl[i]), NULL, philosopher, (void *)(& i));
+		i++;
+	}
+	i = 0;
 	while (i++ < number_of_philosophers)
 	{
-		pthread_create(&(ppl[i]), NULL, philosopher, i);
+		pthread_join(ppl[i], NULL);
 	}
-
 	return (0);
 }
